df8616af134cb4482ddb964ba4b69e076b41b390
DanielDiaz-349 <d.ddandiaz319@gmail.com>
2026-01-02 18:55:24 -0600

    Update main1.py

--- main1.py
+++ main1.py
@@ -88,7 +88,11 @@ def upload_file_to_github(local_path, path_in_repo):
         return False
 
 
-LOGO_UCA_PATH = "Logo_UCA_2015.jpg"
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+LOGO_UCA_PATH = str(BASE_DIR / "assets" / "logo_uca.png")
+
 TEMA_TG = (
     "Introducción a la caracterización y tratamiento matemático del ruido "
     "en sistemas de telecomunicaciones digitales"
